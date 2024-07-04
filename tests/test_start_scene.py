f671438e09638ec1c812c79d3298349141e3aa0f
Matthew Giblett <matthewgiblett@outlook.com>
2024-07-04 22:02:46 +1000

    Added tests dir

---
+++ tests/test_start_scene.py
@@ -0,0 +1,31 @@
+import unittest
+
+import pygame
+
+from src.scenes.start_scene import StartScene
+from tests.tools import minimal_setup
+
+
+class TestStartScene(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        super(TestStartScene, cls).setUpClass()
+        cls.screen = minimal_setup()
+
+    def setUp(self) -> None:
+        self.start_scene = StartScene(self.screen)
+
+    def test_start_experiment(self) -> None:
+        self.assertFalse(self.start_scene.starting)
+        self.assertFalse(self.start_scene.update_state())
+
+        self.start_scene.key_down(pygame.K_RETURN)
+
+        self.assertTrue(self.start_scene.starting)
+        self.assertTrue(self.start_scene.update_state())
+
+    def test_button_down_on_nothing(self) -> None:
+        for mouse_button in range(1, 4):
+            self.start_scene.button_down(mouse_button, (0, 0))
+            self.assertFalse(self.start_scene.starting)
+            self.assertFalse(self.start_scene.update_state())
---
+++ tests/tools.py
@@ -0,0 +1,10 @@
+import pygame
+
+from src.constants import DISPLAY_HEIGHT, DISPLAY_WIDTH
+from src.gui import fonts
+
+
+def minimal_setup() -> pygame.Surface:
+    pygame.init()
+    fonts.init_fonts()
+    return pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
